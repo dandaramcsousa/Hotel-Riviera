51098135de491d615aa51bc3d66d322b2fb81f5d
dandaramcs <dandara.sousa@ccc.ufcg.edu.br>
2015-02-05 15:08:30 -0300

    Javadoc da classe Carro e subclasses.

--- HotelRiviera/Carro/projetolp2/hotelriviera/CarroExecutivo.java
+++ HotelRiviera/Carro/projetolp2/hotelriviera/CarroExecutivo.java
@@ -1,9 +1,18 @@
 package projetolp2.hotelriviera;
 
-
+/**
+ * Classe do tipo Carro. Tipo de carro com diaria de menor valor.
+ * @author Dandara Maria, Pedro Paulo, Paulo Vinicius.
+ *
+ */
 public class CarroExecutivo extends Carro {
 	private final static double DIARIA_CARRO_EXECUTIVO = 60;
 	
+	/**
+	 * Construtor da classe.
+	 * @param tanque Se o carro for alugado com tanque cheio este parametro eh True, caso contrario False.
+	 * @param seguroCarro Se o carro for alugado com seguro este parametro eh True, caso contrario False.
+	 */
 	public CarroExecutivo(boolean tanque, boolean seguroCarro) {
 		super(DIARIA_CARRO_EXECUTIVO, tanque, seguroCarro);
 	}
